bd84482bc60b06b383902354d35434c15ddbde1a
Rushikesh Patil <patilrushikesh8683@gmail.com>
2023-04-10 14:59:16 +0530

    Weekly test4 TODO app

--- src/Componets/InputBox.jsx
+++ src/Componets/InputBox.jsx
@@ -3,41 +3,42 @@ import { GlobalContext } from '../App'
 import '../CSS_files/InputBox.css'
 
 const InputBox = () => {
-    const {currList,setCurrList,currValue,setCurrValue,edit,setEdit}=useContext(GlobalContext)
-    // console.log(edit);
+  const { currList, setCurrList, currValue, setCurrValue, edit, setEdit } = useContext(GlobalContext)
+  // console.log(edit);
   return (
     <>
-    <div className="inputdiv">
-        <input type="text" name="" id="" value={currValue} onChange={(e)=>{
-            setCurrValue(e.target.value)
+      <div className="inputdiv">
+        <input type="text" name="" id="" value={currValue} onChange={(e) => {
+          setCurrValue(e.target.value)
         }} />
-        
-        {edit? <button onClick={()=>{
-          const editValue=currList.filter((ele)=>{
+
+        {edit ?
+         <button onClick={() => {
+          const editValue = currList.filter((ele) => {
             return ele.id === edit
           })
           // console.log(editValue);
           // console.log(currList);
-          const finalEdit=currList.map((e)=>
-          edit===e.id ? (e={id:e.id,currValue}):{id:e.id,currValue:e.currValue}
+          const finalEdit = currList.map((e) =>
+            edit === e.id ? (e = { id: e.id, currValue }) : { id: e.id, currValue: e.currValue }
           )
           console.log(finalEdit);
           setCurrList(finalEdit)
           setEdit(0)
           setCurrValue('')
         }}
+        >Edit</button>
+         :
+          <button onClick={() => {
+            if (currValue !== '') {
+              setCurrList([...currList, { currValue: currValue, id: `${currValue}${Date.now()}` }])
+              setCurrValue('')
+            }
 
-        >edit</button> : <button onClick={()=>{
-           
-          if(currValue !== ''){
-            setCurrList([...currList,{currValue:currValue,id:`${currValue}${Date.now()}`}])
-            setCurrValue('')
-          }
+          }}
 
-        }}
-
-        >go</button>}
-    </div>
+          >ADD</button>}
+      </div>
     </>
   )
 }
